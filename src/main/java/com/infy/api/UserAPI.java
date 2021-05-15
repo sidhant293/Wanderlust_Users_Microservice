afea7ea2e9e616c8d881219f42b3c163e663711f
Sidhant <56150322+sidhant293@users.noreply.github.com>
2021-05-15 22:45:07 +0530

    api registerUser added

--- src/main/java/com/infy/api/UserAPI.java
+++ src/main/java/com/infy/api/UserAPI.java
@@ -5,6 +5,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,16 @@ public class UserAPI {
 
 		try {
 			Users userFromDB = userService.authenticateUser(user.getContactNumber(), user.getPassword());
+			return new ResponseEntity<Users>(userFromDB, HttpStatus.OK);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, environment.getProperty(e.getMessage()));
+		}
+	}
+	
+	@PostMapping(value="/userRegister")
+	public ResponseEntity<Users> registerUser(@RequestBody Users user){
+		try {
+			Users userFromDB = userService.registerUser(user);
 			return new ResponseEntity<Users>(userFromDB, HttpStatus.OK);
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, environment.getProperty(e.getMessage()));
